5fc3f3d3c1b9269a90c649fa043799f9460b813a
Chad Potter <cpotter019@gmail.com>
2024-02-06 14:39:41 -0600

    Added Functionality to buttons

--- hud.gd
+++ hud.gd
@@ -4,6 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$HelpButton.hide()
+	$SoundButton.hide()
+	$ExitButton.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,12 +19,16 @@ func _on_pause_button_toggled(toggled_on):
 		$Heart2.hide()
 		$Heart3.hide()
 		$HelpButton.show()
+		$SoundButton.show()
+		$ExitButton.show()
 	
 	if(toggled_on == false):
 		$Heart.show()
 		$Heart2.show()
 		$Heart3.show()
 		$HelpButton.hide()
+		$SoundButton.hide()
+		$ExitButton.hide()
 
 func _on_help_button_toggled(toggled_on):
 	if(toggled_on == true):
