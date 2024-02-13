8394c964e1b05c5fe8f6fe73cdb8ea6328512247
Julai Dangol <99350702+julaij@users.noreply.github.com>
2024-02-12 23:08:50 -0600

    design a textbox, select font and mount textbox to demo scene

--- Scripts/Dolphin.gd
+++ Scripts/Dolphin.gd
@@ -1,59 +1,20 @@
-extends Node2D
+extends CanvasLayer
 
-var dolphinNode : Node2D
-var textBox : TextEdit
-var outline : Sprite2D
-var closeButton : Button
-var dialogueOptions : Dictionary
-
+@onready var textbox_container = $TextboxContainer
+@onready var label = $TextboxContainer/MarginContainer/HBoxContainer/label
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Assuming you have the Dolphin node, TextBox, TextBoxOutline, and CloseButton added as child nodes
-	dolphinNode = $Dolphin
-	textBox = $TextBox
-	outline = $TextBoxOutline
-	closeButton = $CloseButton
-
-	# Load dialogue options from a dictionary or JSON file
-	dialogueOptions = {
-		1: "Easy dialogue",
-		2: "Medium dialogue",
-		3: "Hard dialogue"
-		# Add more options as needed
-	}
-
-	# Connect the button's pressed signal to the close_textbox function
-	# closeButton.connect("pressed", self, "close_textbox")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
 
-func open_textbox(difficulty_level: int, puddles_content: String) -> void:
-	# Show the necessary nodes
-	dolphinNode.visible = true
-	textBox.visible = true
-	outline.visible = true
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
 
-	# Play dolphin sound
-	play_dolphin_sound()
-
-	# Select dialogue option based on difficulty level
-	var dialogueOption = get_dialogue_option(difficulty_level)
-	textBox.text = dialogueOption.format(puddles_content)
-
-func close_textbox() -> void:
-	# Hide the necessary nodes
-	dolphinNode.visible = false
-	textBox.text = ""
-	outline.visible = false
-
-func play_dolphin_sound() -> void:
-	# Implement the logic to play dolphin sound here
-	# For example, use the AudioStreamPlayer node
-	# assuming you have an AudioStreamPlayer named DolphinSound
-	$DolphinSound.play()
-
-func get_dialogue_option(difficulty_level: int) -> String:
-	# Retrieve the dialogue option based on difficulty level
-	return dialogueOptions.get(difficulty_level, "Default dialogue")
+func hide_textbox():
+	label.text = ""
+	textbox_container.hide()
+	
+func show_textbox():
+	#set dolphin prompt to the label
+	textbox_container.show()
+	#add_prompt(based on conditon)
