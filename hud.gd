127d763e30634a9ba39a2a2eed7a290694ade030
Chad Potter <cpotter019@gmail.com>
2024-02-20 14:03:49 -0600

    Added flask icons

--- hud.gd
+++ hud.gd
@@ -35,10 +35,27 @@ func _on_help_button_toggled(toggled_on):
 		$FlaskHolder.hide()
 		$ProgressBar.hide()
 		$PauseButton.hide()
+		$ExitButton.hide()
+		$SoundButton.hide()
+		$Flask1.hide()
+		$Flask2.hide()
+		$Flask3.hide()
 	
 	if(toggled_on == false):
 		$FlaskHolder.show()
 		$ProgressBar.show()
 		$PauseButton.show()
+		$ExitButton.show()
+		$SoundButton.show()
+		$Flask1.show()
+		$Flask2.show()
+		$Flask3.show()
 	
 	
+
+func _on_exit_button_pressed():
+	get_tree().change_scene_to_file("res://exit.tscn")
+
+
+func _on_help_button_pressed():
+	get_tree().change_scene_to_file("res://help.tscn")
