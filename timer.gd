2467b3c2e55a44e93e80b2cee6a26314bde96e97
grahowe <omgraham02@gmail.com>
2024-03-19 15:16:58 -0500

    Made a basic skeleton timer

---
+++ timer.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@onready var label = %Label
+
+var level_time = 0.0
+var start_level_msec = 5000.0 #300 secs
+
+#func _start():
+#	start_level_msec = Time.get_ticks_msec()
+	
+func _process(delta):
+	
+	level_time = start_level_msec - Time.get_ticks_msec()
+	label.text = "Time Left: " + str(round(level_time / 1000))
+	
