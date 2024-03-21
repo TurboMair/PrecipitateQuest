03ae52ba9f2a2b47a714c589f03935bc248f0690
grahowe <omgraham02@gmail.com>
2024-03-21 14:10:53 -0500

    Timer code

--- timer.gd
+++ timer.gd
@@ -3,7 +3,7 @@ extends Node
 @onready var label = %Label
 
 var level_time = 0.0
-var start_level_msec = 5000.0 #300 secs
+var start_level_msec = 301000.0 #300 secs
 
 #func _start():
 #	start_level_msec = Time.get_ticks_msec()
