83e8bb95f16acbac6a157e655caec98736af0ddb
Haebin Noh <nhb0702@naver.com>
2024-04-11 14:16:22 -0500

    changes

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,6 +2,9 @@ extends Node
 
 var music_playing = true
 var background_music: AudioStreamPlayer2D
+var masterVolume = 1;
+var musicVolume = 1;
+var sfxVolume = 1;
 
 func _ready():
 	background_music = $"background-music"
@@ -62,24 +65,38 @@ func volume(bus_index, value):
 # Master slider
 func _on_master_value_changed(value):
 	volume(0, value)
+	masterVolume = value
 	
 # Music slider
 func _on_music_value_changed(value):
 	volume(1, value)
+	musicVolume = value
 
 # Sound fx slider
 func _on_sfx_value_changed(value):
 	volume(2, value)
+	sfxVolume = value
 
 # Master mute button
-#func _on_optionmaster_box_toggled(toggled_on):
-#	music_playing = not music_playing
-#	if music_playing:
-#		background_music.stream_paused = !background_music.stream_paused
-#		$"sound-effect".play()
-#	else:
-#		background_music.stop()
-#		$"sound-effect".stop()
+func _on_optionmaster_box_toggled(toggled_on):
+	if toggled_on == true:
+		volume(0, 0)
+	else:
+		volume(0, masterVolume)
+
+# Music mute button
+func _on_optionmusic_box_toggled(toggled_on):
+	if toggled_on == true:
+		volume(1, 0)
+	else:
+		volume(1, musicVolume)
+
+# Sound effect mute button
+func _on_optionsfx_box_toggled(toggled_on):
+	if toggled_on == true:
+		volume(2, 0)
+	else:
+		volume(2, sfxVolume)
 	
 # --------------------------------------------------------------
 # Map code
